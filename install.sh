250a9ca473b0e0e9a53d236578eb6cb2b3c6c93a
Ailbhe <51797487+meatpioneer@users.noreply.github.com>
2024-08-24 18:16:17 -0500

    Update install.sh
    Remove wifi section, move to another file

--- install.sh
+++ install.sh
@@ -18,20 +18,6 @@ else
 	echo "continue"
 fi
 
-# # # # > Wi-fi
-# # the wpa_supplicant and dhcpcd service should already be running
-# connect to wifi via wpa_supplicant
-sudo chown ailbhe:ailbhe /etc/wpa_supplicant/wpa_supplicant.conf
-sudo chmod 755 /etc/wpa_supplicant/wpa_supplicant.conf
-wpa_passphrase "IllinoisNet" "password" > /etc/wpa_supplicant/wpa_supplicant.conf
-sed -i '5s/.*/        phase2=\"auth=MSCHAPV2\"/' /etc/wpa_supplicant/wpa_supplicant.conf
-read -p "Username: " username
-echo "        identity=\"$username\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
-read -sp "Password: " password
-echo "        password=\"$password\"" >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo "}" >> /etc/wpa_supplicant/wpa_supplicant.conf
-sudo wpa_supplicant -B -iwlp11s0 -c/etc/wpa_supplicant/wpa_supplicant.conf
-
 # # # # > Bases
 # # # make sure void-linux is up-to-date
 sudo xbps-install --yes -Su
